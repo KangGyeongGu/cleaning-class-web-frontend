e8fc41f24bcb12e32ef51ce58f88865910dc24b1
KangGyeongGu <rudrn0110@naver.com>
2026-03-24 18:32:12 +0900

    feat(seo): robots.txt 네이버 Yeti 크롤러 명시적 허용 추가

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,17 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/admin",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/admin",
+      },
+      {
+        userAgent: "Yeti",
+        allow: "/",
+      },
+    ],
     sitemap: "https://www.cleaningclass.co.kr/sitemap.xml",
   };
 }
